f29375f2d6bf6fbc35d24b4425e50aeab1ea646b
myke amorim <myke.amorim@usp.br>
2023-01-10 14:59:03 -0300

    Added string modifyers module

--- src/main/java/intermediates/string_manipulation/ModifyingStrings.java
+++ src/main/java/intermediates/string_manipulation/ModifyingStrings.java
@@ -3,6 +3,25 @@ package intermediates.string_manipulation;
 public class ModifyingStrings {
 
     public static void main(String[] args) {
-            
+        String test = "This is a test.";
+
+        System.out.println(test.substring(5));
+        System.out.println(test.substring(0, 4));   // Not inclusive.
+
+        String hello = "hello";
+        String world = "world";
+        String helloworld = hello.concat(world);
+        System.out.println(helloworld);
+
+        String wrongSpaces = "s p a z e s";
+        System.out.println(wrongSpaces);
+        String rightSpaces = wrongSpaces.replace('z', 'c');
+
+        String noSpaces = rightSpaces.replaceAll(" ", "");
+        System.out.println(noSpaces);
+
+        String name = " Myke ";
+        System.out.println(name);
+        System.out.println(name.trim());    // Removes lateral spaces.
     }
 }
